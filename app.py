8db0f5c12325b17b3727aab55ec109b77e7ec24c
abhishek1709-vesit <abhishekgore1706@gmail.com>
2025-06-16 21:51:09 +0530

    Ensure db.create_all() runs on Render startup

--- app.py
+++ app.py
@@ -118,7 +118,8 @@ def chat():
     
     return render_template("chat.html")
 
+def create_tables():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
